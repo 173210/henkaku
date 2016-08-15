c48a40a73a0188af014b6985547e9a57aadc5611
173210 <root.3.173210@live.com>
2016-08-15 21:21:07 +0900

    Xamarin: Server: Merge Sever class and Listener class

--- HENkaku/HENkaku/Server/Server.cs
+++ HENkaku/HENkaku/Server/Server.cs
@@ -1,18 +1,51 @@
-﻿namespace HENkaku.Server
+﻿using System;
+using System.Net;
+
+namespace HENkaku.Server
 {
     class Server
     {
-        private readonly Listener listener;
-        
+        private readonly HttpListener listener;
+        private readonly IRoute route;
+
         public Server (string root, string port)
         {
-            var route = new Route (root);
-            listener = new Listener (route, port);
+            listener = new HttpListener();
+            listener.Prefixes.Add("http://*:" + port + "/");
+            route = new Route (root);
+        }
+
+        private void WaitRequestAsync (Log.ILog log)
+        {
+            log.WriteLine ("Waiting for a new request");
+            listener.BeginGetContext (new AsyncCallback (Response), log);
+        }
+
+        private void Response (IAsyncResult result)
+        {
+            var log = (Log.ILog) result.AsyncState;
+
+            try {
+                var context = listener.EndGetContext (result);
+                log.WriteLine ("Serving " + context.Request.RawUrl);
+                var handler = route.GetHandler (context.Request.Url.AbsolutePath);
+                handler.Serve (context);
+            } catch (Exception exception) {
+                log.WriteExceptionWarning (exception);
+            }
+
+            try {
+                WaitRequestAsync (log);
+            } catch (Exception exception) {
+                log.WriteExceptionError (exception);
+                log.WriteLine ("Stopped. Please restart application.");
+            }
         }
 
         public void Start (Log.ILog log)
         {
-            listener.Start (log);
+            listener.Start ();
+            WaitRequestAsync (log);
         }
     }
 }
