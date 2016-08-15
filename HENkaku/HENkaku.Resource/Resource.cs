aee2dde417ad44405a242cfa761ca59a4911d398
173210 <root.3.173210@live.com>
2016-08-15 19:42:05 +0900

    Xamarin: Automate building process

---
+++ HENkaku/HENkaku.Resource/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace HENkaku.Resource
+{
+    public class Resource
+    {
+        public static System.IO.Stream getStream(string name)
+        {
+            var path = "HENkaku.Resource." + name;
+            var assembly = typeof (Resource).GetTypeInfo ().Assembly;
+            return assembly.GetManifestResourceStream (path);
+        }
+    }
+}
--- build_xamarin.bat
+++
@@ -1,9 +0,0 @@
-xcopy /si host\pkg HENkaku\HENkaku\Server\Handler\Resource\pkg
-copy host\exploit.html HENkaku\HENkaku\Server\Handler\Resource\exploit.html
-python3 preprocess.py loader.rop.bin HENkaku\HENkaku\Server\Handler\Resource\stage1.bin
-python3 preprocess.py exploit.rop.bin HENkaku\HENkaku\Server\Handler\Resource\stage2.bin
-convert -geometry 72x72 host\pkg\sce_sys\icon0.png HENkaku\HENkaku.Droid\Resources\drawable-hdpi\icon.png
-convert -geometry 96x96 host\pkg\sce_sys\icon0.png HENkaku\HENkaku.Droid\Resources\drawable-xhdpi\icon.png
-convert -geometry 144x144 host\pkg\sce_sys\icon0.png HENkaku\HENkaku.Droid\Resources\drawable-xxhdpi\icon.png
-copy HENkaku\HENkaku.Droid\Resources\drawable-hdpi\icon.png HENkaku\HENkaku.Droid\Resources\drawable\icon.png
-@echo "done."
